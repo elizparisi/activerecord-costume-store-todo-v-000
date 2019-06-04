8e4b4576689190fa01af15863b62dad181158e24
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-06-04 04:38:41 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -16,19 +16,17 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.string "location"
     t.integer "costume_inventory"
-    t.text "num_of_employees"
-    t.boolean "still_in_business"
-    t.datetime "opening_time"
-    t.datetime "closing_time"
+    t.integer "number_of_employees"
+    t.boolean "in_business"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.integer "price"
-    t.string "size"
+    t.string "string"
     t.text "image_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "haunted_houses", force: :cascade do |t|
@@ -39,7 +37,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.boolean "family_friendly"
     t.datetime "opening_date"
     t.datetime "closing_date"
-    t.text "long_long_description"
+    t.text "long_description"
   end
 
 end
