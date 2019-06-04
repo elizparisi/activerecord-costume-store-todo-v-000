e987e1506cbe48719504e4d4653ac4775b91b68c
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-06-04 04:31:43 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,8 +9,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :costume_inventory 
       t.text :num_of_employees
       t.boolean :still_in_business
-      t.integer :opening_time
-      t.integer :closing_time
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
