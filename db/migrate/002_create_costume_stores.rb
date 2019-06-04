c8dbba6e2fbd300eb900540d604b437b1b01568f
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-06-04 04:18:49 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,5 +4,12 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
   
   def change
     create_table :costume_stores do |t|
-      
+      t.string :name 
+      t.string :location
+      t.integer :costume_inventory 
+      t.integer :number_of_employees
+      t.boolean :in_business
+      t.timestamps
+    end
+  end
 end
